5c5b48ea11baffdd1e8dc43569c0dbf079c3e5ff
ORouse <73557961+ORouse@users.noreply.github.com>
2020-11-24 17:02:24 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,16 @@
 
 Motor left, right;
 Rangefinder rangefinder;
-Servo armServo, armServoFeed;
+Servo armServo;
 ESP32AnalogRead leftLine, rightLine;
 
-
-const float kP = 0.04; // this was tuned on carpet
-const float wheelDiameter = 7; // in cm
+const float kP = 0.04;                                 // this was tuned on carpet
+const float wheelDiameter = 7;                         // in cm
 const float wheelCircumference = 3.14 * wheelDiameter; // in cm
 const float degreesPerCm = 360.0 / wheelCircumference;
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     Motor::allocateTimer(0);
     left.attach(MOTOR_LEFT_PWM, MOTOR_LEFT_DIR, MOTOR_LEFT_ENCA, MOTOR_LEFT_ENCB);
@@ -20,7 +20,6 @@ void setup() {
     rangefinder.attach(SIDE_ULTRASONIC_TRIG, SIDE_ULTRASONIC_ECHO);
     ESP32PWM::allocateTimer(1);
     armServo.attach(33);
-    armServoFeed.attach(34);
     leftLine.attach(36);
     rightLine.attach(39);
 }
@@ -29,7 +28,8 @@ void setup() {
  * Drive straight at 180 deg/sec.
  * @param cm how far to drive, in cm (positive = forward)
  **/
-void forward(float cm) {
+void forward(float cm)
+{
     float deg = cm * degreesPerCm;
     left.startMoveFor(deg, 180);
     right.moveFor(deg, 180);
@@ -39,8 +39,9 @@ void forward(float cm) {
  * Turn in place a certain number of degrees.
  * @param degrees target change in angle, in degrees (positive = clockwise)
  **/
-void turn(float degrees) {
-    float paramDegrees = ((degrees) / 360) * (wheelCircumference) * degreesPerCm * 2;
+void turn(float degrees)
+{
+    float paramDegrees = ((degrees) / 360) * (wheelCircumference)*degreesPerCm * 2;
     left.startMoveFor(paramDegrees, 90);
     right.moveFor(-1 * paramDegrees, 90);
 }
@@ -49,7 +50,8 @@ void turn(float degrees) {
  * Drive with proportional control to target distance and stop.
  * @param distanceFromObject target distance away from object
  **/
-void driveToObject(float distanceFromObject) {
+void driveToObject(float distanceFromObject)
+{
     float error = rangefinder.getDistanceCM() - distanceFromObject;
     left.setEffort(error * kP);
     right.setEffort(error * kP);
@@ -57,7 +59,7 @@ void driveToObject(float distanceFromObject) {
 
 void servootest()
 {
-    
+
     armServo.write(0);
     delay(4000);
     armServo.write(70);
@@ -66,9 +68,8 @@ void servootest()
     delay(4000);
 }
 
-void loop() {
-    
-    Serial.print(armServoFeed.attached());
+void loop()
+{
     servootest();
     delay(500);
 }
