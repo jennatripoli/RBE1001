3de83993fd3ba56ddf48973f9ad1662eec15082a
jennatripoli <jennamt1282@gmail.com>
2020-12-01 16:41:53 -0800

    Cleaning up for submission

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 
 Motor left, right;
 Rangefinder rangefinder;
-//Servo armServo;
-//ESP32AnalogRead leftLine, rightLine;
 
 const float kP = 0.02;
 const float wheelDiameter = 7; // in cm
@@ -14,13 +12,9 @@ const float degreesPerCm = 360.0 / wheelCircumference;
 void setup() {
     Serial.begin(9600);
     Motor::allocateTimer(0);
-    //ESP32PWM::allocateTimer(1);
     left.attach(MOTOR_LEFT_PWM, MOTOR_LEFT_DIR, MOTOR_LEFT_ENCA, MOTOR_LEFT_ENCB);
     right.attach(MOTOR_RIGHT_PWM, MOTOR_RIGHT_DIR, MOTOR_RIGHT_ENCA, MOTOR_RIGHT_ENCB);
     rangefinder.attach(SIDE_ULTRASONIC_TRIG, SIDE_ULTRASONIC_ECHO);
-    //armServo.attach(33);
-    //leftLine.attach(36);
-    //rightLine.attach(39);
 }
 
 /**
@@ -47,19 +41,14 @@ void turn(float degrees) {
  * Drive with proportional control to target distance and stop.
  * @param distanceFromObject target distance away from object, in cm
  **/
-
 void driveToObject(float distanceFromObject) {
-    Serial.print("Help");
-    while (rangefinder.getDistanceCM() >= 5.08)
-    {
-    float error = rangefinder.getDistanceCM() - distanceFromObject;
-    left.setEffort(error * kP * 1.15);
-    right.setEffort(error * kP);
+    while (rangefinder.getDistanceCM() >= 5.08) {
+        float error = rangefinder.getDistanceCM() - distanceFromObject;
+        left.setEffort(error * kP * 1.15); // multiply by 1.15 because otherwise bot does not drive straight
+        right.setEffort(error * kP);
     }
     left.setEffort(0);
     right.setEffort(0);
-    
-    
 }
 
 /**
@@ -67,28 +56,18 @@ void driveToObject(float distanceFromObject) {
  * @param distanceFromObject max distance away from object, in cm
  **/
 void turnToObject(float distanceFromObject) {
-    Serial.print("help");
     while (rangefinder.getDistanceCM() > distanceFromObject) { // while object is out of range
         left.setEffort(0.2);
         right.setEffort(-0.2);
-        Serial.print("asdf");
     }
     float leftEdge = right.getCurrentDegrees(), rightEdge = leftEdge; // default for rightEdge causes no turning
     while (rangefinder.getDistanceCM() < distanceFromObject) {} // wait for the object to get out of range
     if (rangefinder.getDistanceCM() > distanceFromObject) rightEdge = right.getCurrentDegrees(); // when object is out of range
-    Serial.print(leftEdge);
-    Serial.print(rightEdge);
     turn((rightEdge - leftEdge) / 4); // turn ccw to center of object (average between the two edges)
-    
 }
 
-void loop() 
-{
-
+void loop() {
     delay(1000);
     turnToObject(30);
     driveToObject(5.08);
-    
-
-    
 }
